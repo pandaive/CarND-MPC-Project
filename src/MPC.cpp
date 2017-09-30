95148d3e45546c8941bbc3e7aa398ae98fde4a2d
kzon <kzon@nexway.com>
2017-09-30 22:50:36 +0200

    Final parameters

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,8 +50,8 @@ class FG_eval {
     
     // Reference State Cost
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += 100*CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 2000*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 1000*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
